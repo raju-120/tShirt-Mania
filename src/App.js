47507f7ec1928c3ccf41a1150ed29a15ecf3f421
Raju <ashfaqraju27@gmail.com>
2022-10-10 22:42:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import Main from './Layout/Main';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Orders from './Components/Orders/Orders';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Main></Main>,
+      children: [
+        {
+          path:'/',
+          loader: () => fetch('tshirt.json'),
+          element: <Home></Home>
+        },
+        {
+          path:'/orders',
+          element: <Orders></Orders>
+        }
+      ]
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import './Home.css';
+
+const Home = () => {
+    const tshirts = useLoaderData();
+    return (
+        <div className='home-container'>
+            <div className='t-shirt-container'>
+            {
+                tshirts.map(tshirt => <TShirt
+                key={tshirt._id}
+                tshirt={tshirt}>
+
+                </TShirt>)       
+            }
+            </div>
+            <div className='cart-container'>
+                <Cart ></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
